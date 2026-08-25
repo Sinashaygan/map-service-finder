ab20efb203340545d50d7fcdb0ed67ef0ce8e423
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-25 10:21:35 +0330

    feat: add the ui slice and reducers

--- src/store/ui-slice.ts
+++ src/store/ui-slice.ts
@@ -1,3 +1,5 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
 type ViewMode = "map" | "list";
 
 interface UiState {
@@ -11,3 +13,26 @@ const initialState: UiState = {
   isDetailsPanelOpen: false,
   isFilterPanelOpen: false,
 };
+
+const uiSlice = createSlice({
+  name: "ui",
+  initialState,
+  reducers: {
+    setViewMode(state, action: PayloadAction<ViewMode>) {
+      state.viewMode = action.payload;
+    },
+
+    toggleDetailsPanel(state) {
+      state.isDetailsPanelOpen = !state.isDetailsPanelOpen;
+    },
+
+    toggleFilterPanel(state) {
+      state.isFilterPanelOpen = !state.isFilterPanelOpen;
+    },
+  },
+});
+
+export const { setViewMode, toggleDetailsPanel, toggleFilterPanel } =
+  uiSlice.actions;
+
+export default uiSlice.reducer;
