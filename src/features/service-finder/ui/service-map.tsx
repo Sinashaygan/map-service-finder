4922afc2894f1dff65e2c03958698937220c3f97
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-25 14:35:28 +0330

    feat: add the effect to go to the selected location

--- src/features/service-finder/ui/service-map.tsx
+++ src/features/service-finder/ui/service-map.tsx
@@ -1,4 +1,7 @@
+"use client"
 import { Service } from "@/entities/service/model/types";
+import { useEffect } from "react";
+import { useMap } from "react-leaflet";
 
 const TEHRAN_CENTER: [number, number] = [35.7219, 51.3347];
 const DEFAULT_ZOOM = 12;
@@ -13,5 +16,17 @@ export default function ServiceMap({
   services,
   selectedServiceId,
 }: Omit<ServiceMapProps, "onSelectService">) {
+    const map = useMap();
+
+    useEffect(() => {
+      if (!selectedServiceId) return;
+      const target = services.find((s) => s.id === selectedServiceId);
+      if (target) {
+        map.flyTo([target.location.lat, target.location.lng], 15, {
+          duration: 0.8,
+        });
+      }
+    }, [map, services, selectedServiceId]);
+
   return <div>service-map</div>;
 }
