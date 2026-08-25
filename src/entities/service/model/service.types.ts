bd7f590cea24dca6bb897f5e7b6a076bd439aef3
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-25 11:15:27 +0330

    feat: add the types of services

---
+++ src/entities/service/model/service.types.ts
@@ -0,0 +1,10 @@
+export type ServiceCategory =
+  | "restaurant"
+  | "cafe"
+  | "pharmacy"
+  | "hospital"
+  | "mechanic"
+  | "hotel"
+  | "coworking";
+
+  export type PriceLevel = 1 | 2 | 3 | 4;
