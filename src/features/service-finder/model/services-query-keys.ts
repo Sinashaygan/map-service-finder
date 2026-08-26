45c59c7e050a859a723adb66ac41b2125a3a90de
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-26 11:47:34 +0330

    Refactor: the query keys for adding filters for the list of services

--- src/features/service-finder/model/services-query-keys.ts
+++ src/features/service-finder/model/services-query-keys.ts
@@ -1,6 +1,9 @@
+import { ServiceFilters } from "@/entities/service/model/filters";
+
 export const serviceKeys = {
   all: ["services"] as const,
-  list: () => [...serviceKeys.all, "list"] as const,
+  list: (filters: ServiceFilters) =>
+    [...serviceKeys.all, "list", filters] as const,
   detail: (id: string) => [...serviceKeys.all, "detail", id] as const,
 };
 
