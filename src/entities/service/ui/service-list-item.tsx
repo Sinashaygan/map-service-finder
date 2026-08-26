c95beb1f01b014a444d8fa542d612a6483bb0d21
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-26 23:51:09 +0330

    feat: update the services-list components with skeleton and new list item

--- src/entities/service/ui/service-list-item.tsx
+++ src/entities/service/ui/service-list-item.tsx
@@ -11,7 +11,7 @@ interface ServiceListItemProps {
   onHoverChange: (hovered: boolean) => void;
 }
 
-function ServiceListItem({
+export function ServiceListItem({
   service,
   isSelected,
   isHovered,
