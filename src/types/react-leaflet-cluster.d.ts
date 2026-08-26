32d1da019d0aa397c6696eae62454a2cc68c1f0f
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-26 09:43:22 +0330

    Refactor: the cluster interface of LayerGroupProps

--- src/types/react-leaflet-cluster.d.ts
+++ src/types/react-leaflet-cluster.d.ts
@@ -1,21 +1,21 @@
 declare module "react-leaflet-cluster" {
+  import type { ComponentType } from "react";
   import type { LayerGroupProps } from "react-leaflet";
-  import type { Icon, Marker, DivIcon } from "leaflet";
+  import type { DivIcon, Icon, Marker } from "leaflet";
 
   interface MarkerClusterGroupProps extends LayerGroupProps {
     maxClusterRadius?: number;
     spiderfyOnMaxZoom?: boolean;
     showCoverageOnHover?: boolean;
     zoomToBoundsOnClick?: boolean;
-    getChileClusteringAtZoom?: number;
+    disableClusteringAtZoom?: number;
     iconCreateFunction?: (cluster: {
       getChildCount: () => number;
       getAllChildMarkers: () => Marker[];
     }) => Icon | DivIcon;
     polylineOptions?: Record<string, unknown>;
-    disableClusteringAtZoom?:number;
   }
 
-  const MarkerClusterGroup: React.ComponentType<MarkerClusterGroupProps>;
+  const MarkerClusterGroup: ComponentType<MarkerClusterGroupProps>;
   export default MarkerClusterGroup;
 }
