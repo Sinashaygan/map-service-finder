a8ed608ac0d6943440f5fd96f61b228f9c2e5dd6
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-26 11:17:28 +0330

    feat: add the filter slice to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import { selectionReducer } from "./selection-slice";
 import uiSlice from "./ui-slice";
 import geoSlice from "./geo-slice";
+import { filtersReducer } from "./filters-slice";
 
 export const store = configureStore({
   reducer: {
     selection: selectionReducer,
     ui: uiSlice,
     geo: geoSlice,
+    filters:filtersReducer
   },
 });
 
