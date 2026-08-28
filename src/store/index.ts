87f5bc5999a45e85578e7b883ecc017a91f87f6c
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-28 16:46:28 +0330

    Delete : the unused slice of redux

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { selectionReducer } from "./selection-slice";
-import uiSlice from "./ui-slice";
 import { filtersReducer } from "./filters-slice";
 import { geoSlice } from "./geolocation-slice";
 import { spatialFilterReduce } from "./spatial-filter-slice";
@@ -8,7 +7,6 @@ import { spatialFilterReduce } from "./spatial-filter-slice";
 export const store = configureStore({
   reducer: {
     selection: selectionReducer,
-    ui: uiSlice,
     geolocation: geoSlice,
     filters: filtersReducer,
     spatialFilter: spatialFilterReduce,
